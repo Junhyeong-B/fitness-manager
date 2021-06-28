fde12c063150986543cfe15b85b69a399a0ec7d6
Bae-Junhyeong <85148549+Junhyeong-B@users.noreply.github.com>
2021-06-28 11:32:56 +0900

    version 0.5.3.2
    stretching 사이트에서 의도한 3가지 정보의 이미지를 클릭하면 해당 내용이 popup 되는 모달 추가.

--- stretching.js
+++ stretching.js
@@ -1,10 +1,36 @@
-const openModal = document.querySelector(".first__img")
-const closeModal = document.getElementById("close__modal");
+// 첫 번째 모달 창
+const openModal1 = document.querySelector(".first__img")
+const closeModal1 = document.querySelector(".close__modal1");
 
-openModal.addEventListener("click", () => {
+openModal1.addEventListener("click", () => {
     document.querySelector(".modal__first").classList.add("active");
 });
 
-closeModal.addEventListener("click", () => {
+closeModal1.addEventListener("click", () => {
     document.querySelector(".modal__first").classList.remove("active");
+});
+
+// 두 번째 모달 창
+const openModal2 = document.querySelector(".second__img")
+const closeModal2 = document.querySelector(".close__modal2");
+
+openModal2.addEventListener("click", () => {
+    document.querySelector(".modal__second").classList.add("active");
+});
+
+closeModal2.addEventListener("click", () => {
+    document.querySelector(".modal__second").classList.remove("active");
+});
+
+
+// 세 번째 모달 창
+const openModal3 = document.querySelector(".third__img")
+const closeModal3 = document.querySelector(".close__modal3");
+
+openModal3.addEventListener("click", () => {
+    document.querySelector(".modal__third").classList.add("active");
+});
+
+closeModal3.addEventListener("click", () => {
+    document.querySelector(".modal__third").classList.remove("active");
 });
