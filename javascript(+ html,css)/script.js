f9e210a5f5103091ad114f2b2886240f10658cff
Bae-Junhyeong <85148549+Junhyeong-B@users.noreply.github.com>
2021-07-05 20:18:31 +0900

    Delete javascript(+ html,css) directory
    folder 세분화를 위해 directory 삭제.

--- javascript(+ html,css)/script.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.main__toggleBtn');
-const menu = document.querySelector('.toggle__links ul');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
