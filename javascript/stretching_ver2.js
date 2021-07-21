3c400ab5ca5dcbb44c1f26348ef92a937aaf0a8e
Bae-Junhyeong <85148549+Junhyeong-B@users.noreply.github.com>
2021-07-21 16:07:50 +0900

    version 0.5.6.3
    Stretching 페이지에서 Label을 이용한 CSS 슬라이드 에서 Javascript + Button 을 이용한 Slide로 수정하여 유지보수에 더 용이하게 변경(차후 Prev, Next 버튼 생성 및 Animation 효과 추가 예정)

---
+++ javascript/stretching_ver2.js
@@ -0,0 +1,80 @@
+// // 첫 번째 모달 창
+// const openModal1 = document.querySelector(".first__content")
+// const closeModal1 = document.querySelector(".close__modal1");
+
+// openModal1.addEventListener("click", () => {
+//     document.querySelector(".modal__first").classList.add("active");
+// });
+
+// closeModal1.addEventListener("click", () => {
+//     document.querySelector(".modal__first").classList.remove("active");
+// });
+
+// // 두 번째 모달 창
+// const openModal2 = document.querySelector(".second__content")
+// const closeModal2 = document.querySelector(".close__modal2");
+
+// openModal2.addEventListener("click", () => {
+//     document.querySelector(".modal__second").classList.add("active");
+// });
+
+// closeModal2.addEventListener("click", () => {
+//     document.querySelector(".modal__second").classList.remove("active");
+// });
+
+
+// // 세 번째 모달 창
+// const openModal3 = document.querySelector(".third__content")
+// const closeModal3 = document.querySelector(".close__modal3");
+
+// openModal3.addEventListener("click", () => {
+//     document.querySelector(".modal__third").classList.add("active");
+// });
+
+// closeModal3.addEventListener("click", () => {
+//     document.querySelector(".modal__third").classList.remove("active");
+// });
+
+// Button Click 시 해당 버튼에 해당하는 이미지 표시
+const btn1 = document.querySelector(".btn1")
+const btn2 = document.querySelector(".btn2")
+const btn3 = document.querySelector(".btn3")
+const SHOWING_CLASS = "showing";
+
+btn1.addEventListener("click", () => {
+    if (btn1.classList[1] !== SHOWING_CLASS) {
+        document.querySelector(".li2").classList.remove(SHOWING_CLASS);
+        document.querySelector(".li3").classList.remove(SHOWING_CLASS);
+        document.querySelector(".li1").classList.add(SHOWING_CLASS);
+
+        btn2.classList.remove(SHOWING_CLASS);
+        btn3.classList.remove(SHOWING_CLASS);
+        btn1.classList.add(SHOWING_CLASS);
+    }
+});
+
+btn2.addEventListener("click", () => {
+    if (btn2.classList[1] !== SHOWING_CLASS) {
+        document.querySelector(".li1").classList.remove(SHOWING_CLASS);
+        document.querySelector(".li3").classList.remove(SHOWING_CLASS);
+        document.querySelector(".li2").classList.add(SHOWING_CLASS);
+
+        btn1.classList.remove(SHOWING_CLASS);
+        btn3.classList.remove(SHOWING_CLASS);
+        btn2.classList.add(SHOWING_CLASS);
+    }
+});
+
+btn3.addEventListener("click", () => {
+    if (btn3.classList[1] !== SHOWING_CLASS) {
+        document.querySelector(".li1").classList.remove(SHOWING_CLASS);
+        document.querySelector(".li2").classList.remove(SHOWING_CLASS);
+        document.querySelector(".li3").classList.add(SHOWING_CLASS);
+
+        btn1.classList.remove(SHOWING_CLASS);
+        btn2.classList.remove(SHOWING_CLASS);
+        btn3.classList.add(SHOWING_CLASS);
+    }
+});
+
+
