a1331b1ef95c95ec3d8e88486982919a4f28e1fb
Bae-Junhyeong <85148549+Junhyeong-B@users.noreply.github.com>
2021-07-24 00:27:30 +0900

    version 0.5.6.6
    Stretching 페이지에 다음, 이전으로 넘어가는 버튼 추가.(기존 버튼은 이미지 따라 움직였으나 Javascript를 이용하는 방법으로 변경하여 고정)

--- javascript/stretching_ver2.js
+++ javascript/stretching_ver2.js
@@ -1,41 +1,4 @@
-// // 첫 번째 모달 창
-const openModal1 = document.querySelector(".first__content")
-const closeModal1 = document.querySelector(".close__modal1");
-
-openModal1.addEventListener("click", () => {
-    document.querySelector(".modal__first").classList.add("active");
-});
-
-closeModal1.addEventListener("click", () => {
-    document.querySelector(".modal__first").classList.remove("active");
-});
-
-// // 두 번째 모달 창
-const openModal2 = document.querySelector(".second__content")
-const closeModal2 = document.querySelector(".close__modal2");
-
-openModal2.addEventListener("click", () => {
-    document.querySelector(".modal__second").classList.add("active");
-});
-
-closeModal2.addEventListener("click", () => {
-    document.querySelector(".modal__second").classList.remove("active");
-});
-
-
-// // 세 번째 모달 창
-const openModal3 = document.querySelector(".third__content")
-const closeModal3 = document.querySelector(".close__modal3");
-
-openModal3.addEventListener("click", () => {
-    document.querySelector(".modal__third").classList.add("active");
-});
-
-closeModal3.addEventListener("click", () => {
-    document.querySelector(".modal__third").classList.remove("active");
-});
-
-// Button Click 시 해당 버튼에 해당하는 이미지 표시
+// 아래 쪽 Button Click 시 해당 버튼에 해당하는 이미지 표시
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const btn3 = document.querySelector(".btn3");
@@ -82,4 +45,119 @@ btn3.addEventListener("click", () => {
         btn2.classList.remove(SHOWING_CLASS);
         btn3.classList.add(SHOWING_CLASS);
     }
+});
+
+// <, > 화살표 버튼 클릭 시 이미지 이동
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+// NEXT 버튼 클릭 시(">")
+next.addEventListener("click", () => {
+    if (slides[0].classList.contains("showing1")) {
+        for (let x of slides) {
+            x.classList.remove("showing1");
+            x.classList.remove("showing3");
+            x.classList.add("showing2");
+        };
+
+        btn1.classList.remove(SHOWING_CLASS);
+        btn3.classList.remove(SHOWING_CLASS);
+        btn2.classList.add(SHOWING_CLASS);
+    }
+    else if (slides[0].classList.contains("showing2")) {
+        for (let x of slides) {
+            x.classList.remove("showing1");
+            x.classList.remove("showing2");
+            x.classList.add("showing3");
+        };
+
+        btn1.classList.remove(SHOWING_CLASS);
+        btn2.classList.remove(SHOWING_CLASS);
+        btn3.classList.add(SHOWING_CLASS);
+    }
+    else if (slides[0].classList.contains("showing3")) {
+        for (let x of slides) {
+            x.classList.remove("showing2");
+            x.classList.remove("showing3");
+            x.classList.add("showing1");
+        };
+
+        btn2.classList.remove(SHOWING_CLASS);
+        btn3.classList.remove(SHOWING_CLASS);
+        btn1.classList.add(SHOWING_CLASS);
+    }
+});
+
+// PREV 버튼 클릭 시("<")
+prev.addEventListener("click", () => {
+    if (slides[0].classList.contains("showing1")) {
+        for (let x of slides) {
+            x.classList.remove("showing1");
+            x.classList.remove("showing2");
+            x.classList.add("showing3");
+        };
+
+        btn1.classList.remove(SHOWING_CLASS);
+        btn2.classList.remove(SHOWING_CLASS);
+        btn3.classList.add(SHOWING_CLASS);
+    }
+    else if (slides[0].classList.contains("showing2")) {
+        for (let x of slides) {
+            x.classList.remove("showing2");
+            x.classList.remove("showing3");
+            x.classList.add("showing1");
+        };
+
+        btn2.classList.remove(SHOWING_CLASS);
+        btn3.classList.remove(SHOWING_CLASS);
+        btn1.classList.add(SHOWING_CLASS);
+    }
+    else if (slides[0].classList.contains("showing3")) {
+        for (let x of slides) {
+            x.classList.remove("showing1");
+            x.classList.remove("showing3");
+            x.classList.add("showing2");
+        };
+
+        btn1.classList.remove(SHOWING_CLASS);
+        btn3.classList.remove(SHOWING_CLASS);
+        btn2.classList.add(SHOWING_CLASS);
+    }
+});
+
+// 첫 번째 모달 창
+const openModal1 = document.querySelector(".first__content")
+const closeModal1 = document.querySelector(".close__modal1");
+
+openModal1.addEventListener("click", () => {
+    document.querySelector(".modal__first").classList.add("active");
+});
+
+closeModal1.addEventListener("click", () => {
+    document.querySelector(".modal__first").classList.remove("active");
+});
+
+// 두 번째 모달 창
+const openModal2 = document.querySelector(".second__content")
+const closeModal2 = document.querySelector(".close__modal2");
+
+openModal2.addEventListener("click", () => {
+    document.querySelector(".modal__second").classList.add("active");
+});
+
+closeModal2.addEventListener("click", () => {
+    document.querySelector(".modal__second").classList.remove("active");
+});
+
+
+// 세 번째 모달 창
+const openModal3 = document.querySelector(".third__content")
+const closeModal3 = document.querySelector(".close__modal3");
+
+openModal3.addEventListener("click", () => {
+    document.querySelector(".modal__third").classList.add("active");
+});
+
+closeModal3.addEventListener("click", () => {
+    document.querySelector(".modal__third").classList.remove("active");
 });
