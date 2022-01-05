6a4534bffaccc8991dd706ac4d713026f962f5c7
JunHyeongB <aowlr0513@gmail.com>
2022-01-05 22:35:18 +0900

    chore: Typescript Alias 설정

---
+++ craco.config.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line
+const CracoAlias = require('craco-alias');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: '.',
+        tsConfigPath: 'tsconfig.paths.json',
+        debug: false,
+      },
+    },
+  ],
+};
