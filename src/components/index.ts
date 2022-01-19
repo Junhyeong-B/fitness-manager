73e4dc8a84cbffea5babc923f6051cfc0adbe80c
Bae-Junhyeong <85148549+Junhyeong-B@users.noreply.github.com>
2022-01-19 23:40:32 +0900

    Revert "Modal 컴포넌트 추가"

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,3 @@ export { Nav } from './Nav';
 export { MainMenuImage } from './MainMenuImage';
 export { WorkoutInfo } from './WorkoutInfo';
 export { Slider } from './Slider';
-export { Portal } from './Portal';
-export { Modal } from './Modal';
